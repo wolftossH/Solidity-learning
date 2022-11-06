eade862eef501e63e49feaa3cc5bef4a20559891
wolftossH <57511507+wolftossH@users.noreply.github.com>
2022-11-06 13:30:20 -0800

    Update campaign.sol

--- campaign.sol
+++ campaign.sol
@@ -2,29 +2,74 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
+contract CampaignFactory {
+    address[] public deployedCampaigns;
+
+    function createCampaign(uint minimum) public {
+        address newCampaign = new Campaign(minimum, msg.sender);
+        deployedCampaigns.push(newCampaign);
+    }
+
+    function getDeployedCampaigns() public view returns (address[]) {
+        return deployedCampaigns;
+
+    }
+}
+
 contract Campaign {
     struct Request {
         string description;
         uint value;
         address recipient;
         bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
     }
 
     Request[] public requests;
     address public manager;
     uint public minimumContribution;
-    address[] public approvers;
+    // address[] public approvers;
+    mapping(address => bool) public approvers;
+    uint public approversCount;
 
-    constructor(uint minimum) public {
-        manager = msg.sender;
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+    constructor(uint minimum, address creator) public {
+        manager = creator;
         minimumContribution = minimum;
     }
 
     function contribute() public payable {
         require(msg.value>minimumContribution);
-        approvers.push(msg.sender);
+        // approvers.push(msg.sender);
+        approvers[msg.sender] = true;
+        approversCount++;
     }
 
+    function approveRequest(uint index) public {
+        // change directly in memory address
+        Request storage request = requests[index];
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]);
+
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+
+    function finalizeRequest(uint index) public restricted{
+        Request storage request = requests[index];
+
+        // more than half of the population to finalize the request
+        require(request.approvalCount>approversCount/2);
+        require(!request.complete);
+
+        request.recipient.transfer(request.value);
+        request.complete = true;
+    }
 
 
 
